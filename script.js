b97ca479ee302b411e6cc6467af191271fc77043
Smit Raut <raut.smit1202@gmail.com>
2023-08-29 12:22:09 +0530

    third commit

--- script.js
+++ script.js
@@ -3,15 +3,28 @@ let redDiv = document.getElementById('red')
 let yellowDiv = document.getElementById('yellow')
 let greenDiv = document.getElementById('green')
 
-const squares = document.querySelectorAll('.colorSquare')
 
+//counter
+const squares = document.querySelectorAll('.colorSquare')
 const timesClicked = {'red': 0, 'yellow': 0, 'green': 0 }
 
 squares.forEach(square => {
     square.onclick = () => {
         timesClicked[square.value] +=1
-        square.innerText = timesClicked[square.value]
-    
-       
+        square.innerText = timesClicked[square.value] 
     } 
 })
+
+//reset game
+const clearScores = () => {
+    timesClicked.red = 0
+    timesClicked.yellow = 0
+    timesClicked.green = 0
+    squares.forEach(square => {
+        square.innerText = 0
+    })
+
+}
+
+const reset = document.getElementById('reset')
+reset.onclick = () => clearScores()
